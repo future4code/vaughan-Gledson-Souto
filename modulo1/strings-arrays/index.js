8b88b6e67dd9a28d7bfbeb6c63eb36769b0a6349
GledsonLucas111 <gledsonlucas111@gmail.com>
2021-12-01 15:21:21 -0300

    Strings e Arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,61 @@
+// Exercícios de interpretação de código
+
+// Exercico 1
+// 1 a.undefined  b.null  c.11  d.3  e.[3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]  f.9
+
+// Exercicio 2 
+// A frase ficará assim: SUBI NO ÔNIBUS EM MIRRICOS 
+//26
+
+//Exercícios de escrita de código
+//Exercicio 1
+
+// const nome = prompt ("Qual seu nome? ");
+// const emailUsuario = prompt ("Digite seu e-mail: ");
+
+// console.log(`O e-mail: ${emailUsuario} nfoi cadastrado com sucesso. Seja bem-vindo(a) ${nome}`);
+
+//Exercicio 2
+ 
+// let frutas = ["Acerola","Banana","Abacate","Maça","Laranja"];
+
+ //a)
+// console.log(frutas);
+
+ //b)
+// console.log("Essas são as minhas comidas preferidas: ")
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+// console.log(frutas[3]);
+// console.log(frutas[4]);
+
+ //c)
+
+// let comida = prompt ("Qual sua comida favorita? ");
+
+// frutas[1] = comida;
+
+// console.log(frutas);
+
+//Exercicio 3
+//
+// const listaDeTarefas = [];
+
+// alert("Diga 3 tarefas que você precise realizar hoje: ");
+
+// const TarefasUsuario1 = prompt ("Digite a primeira tarefa ");
+// const TarefasUsuario2 = prompt ("Digite a  segunda tarefa ");
+// const TarefasUsuario3 = prompt ("Digite a terceira tarefa ");
+
+// listaDeTarefas.push(TarefasUsuario1);
+// listaDeTarefas.push(TarefasUsuario2);
+// listaDeTarefas.push(TarefasUsuario3);
+
+// console.log(listaDeTarefas);
+
+// const indeciDeTarefa = prompt ("digite o índice de uma tarefa que você já realizou: 0, 1 ou 2?");
+
+// listaDeTarefas.splice(indeciDeTarefa,1);
+
+// console.log(listaDeTarefas);
