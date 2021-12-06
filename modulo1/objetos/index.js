f829904ab30285294828ea43bd650ebf0fcfb591
GledsonLucas111 <gledsonlucas111@gmail.com>
2021-12-06 15:50:25 -0300

    Objetos JS

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,69 @@
+//Exercícios de interpretação de código
+//Exercicio 1
+// a) Matheus Nachtergaele, Virginia Cavendish, Canal: Globo  horario: 14h
+
+
+//Exercicio 2
+// a) nome: Juca idade: 3 raca: SRD,
+//Nome: Juba idade: 3 raca: SRD,
+//Nome: Jubo idade: 3 raca: SRD 
+
+//b) ele faz uma copia do objeto, podendo ser alterado de acordo com o que a pessoa colocar
+
+
+//Exercicio 3
+//a) False, undefined
+
+// b) por não estarem ligadas as informações continuaram as mesmas,
+//dando indefinido por não conter altura também
+
+
+//Exercícios de escrita de código
+//Exercicio 1
+// a)
+ 
+// const pessoa = {
+//     nome: "Lucas",
+//     apelidos: ["Luquinhas", "Luke","Lu"]
+// }
+// console.log(`Eu sou ${pessoa.nome}, 
+// mas pode me chamar de: ${pessoa.apelidos[0]}, ${pessoa.apelidos[1]} ou ${pessoa.apelidos[2]}`)
+// // b)
+// const novos_apelidos = {
+//     ...pessoa,
+//     apelidos: ["Lulu", "Luca","Luluquinhas"]
+// }
+// console.log(novos_apelidos)
+
+//Exercicio 2
+
+// const pessoa = {
+//     nome:"Lucas" ,
+//     idade:20 ,
+//     profissao: "FrontEnder"
+// }
+// const DadosOutraPessoa = [pessoa.nome, pessoa.nome.length, pessoa.idade, 
+//     pessoa.profissao, pessoa.profissao.length]
+    
+// console.log(DadosOutraPessoa)
+
+
+//Exercicio 3
+
+// const carrinho = []
+
+// const sacolao1 = {
+//     nome: "Morango",
+//     disponibilidade: true
+// }
+// const sacolao2 = {
+//     nome: "Banana",
+//     disponibilidade: true
+// }
+// const sacolao3 = {
+//     nome: "Uva",
+//     disponibilidade: true
+// }
+
+// carrinho.push(sacolao1, sacolao2, sacolao3)
+// console.log(carrinho)
