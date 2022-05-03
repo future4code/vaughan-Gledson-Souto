531852f71b132bb6c383d403cf3528ece7c87eaf
Gledson Lucas <gledsonlucas111@gmail.com>
2022-05-03 18:02:34 -0300

    labook

---
+++ modulo5/LabEbook/src/services/Authenticator.ts
@@ -0,0 +1,28 @@
+import * as jwt from "jsonwebtoken";
+
+type authenticationData = {
+    id: string
+}
+
+export class Authenticator {
+   static generateToken = (
+      payload: authenticationData
+   ): string => {
+      return jwt.sign(
+         payload,
+         process.env.JWT_KEY as string,
+         {
+            expiresIn: "24min"
+         }
+      )
+   }
+
+   static getTokenData = (
+      token: string
+   ): authenticationData => {
+      return jwt.verify(
+         token,
+         process.env.JWT_KEY as string
+      ) as authenticationData
+   }
+}
